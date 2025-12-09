b500ee0358c3fe146d5485c10bdb4133d9ab5f88
sanjaymalik1 <msanjay31103@gmail.com>
2025-12-09 13:50:42 +0530

    fixed responsiveness issue for medium screens

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -5,12 +5,12 @@ import { initialBoard } from '../lib/gameLogic';
 export default function Board({board}) {
 
     return (
-        <div className='h-full w-full bg-gray-800 rounded-2xl md:rounded-3xl 2xl:rounded-t-4xl pt-1 px-1 md:pt-1.5 md:px-1.5 2xl:pt-2 2xl:px-2'>
+        <div className='h-full w-full bg-gray-800 rounded-2xl md:rounded-3xl xl:rounded-t-4xl pt-1 px-1 md:pt-1.5 md:px-1.5 xl:pt-2 xl:px-2'>
             <div className='grid grid-cols-7 grid-rows-6'>
                 {board.map((row, rowId) =>
                     row.map((cell, cellId) => (
-                        <div key={`${rowId}-${cellId}`} className='w-10 h-10 md:w-14 md:h-14 2xl:w-20 2xl:h-20 flex items-center justify-center'>
-                            <div className={`${cell === "A" && "bg-red-700"} ${cell=== "B" &&  "bg-yellow-500"} bg-gray-950 md:w-12 md:h-12 2xl:w-18 2xl:h-18 w-8 h-8 border rounded-full`}>
+                        <div key={`${rowId}-${cellId}`} className='w-10 h-10 md:w-14 md:h-14 xl:w-20 xl:h-20 flex items-center justify-center'>
+                            <div className={`${cell === "A" && "bg-red-700"} ${cell=== "B" &&  "bg-yellow-500"} bg-gray-950 md:w-12 md:h-12 xl:w-18 xl:h-18 w-8 h-8 border rounded-full`}>
                         </div>
                         </div>
                     ))
--- app/components/CurrentPlayerDisplay.jsx
+++ app/components/CurrentPlayerDisplay.jsx
@@ -12,10 +12,10 @@ export default function CurrentPlayerDisplay({ player, color, handleChance, aler
     }
 
     return (
-        <div className={`absolute md:top-12 md:left-1/2 md:-translate-x-1/2 2xl:top-18 2xl:left-auto 2xl:translate-x-0 2xl:right-6 md:w-80 2xl:w-92 flex flex-col items-center border border-gray-500 md:rounded-xl 2xl:rounded-2xl md:py-6 2xl:py-8 md:pb-16 2xl:pb-22  w-68 py-4 pb-12 top-16 rounded-xl`}>
-            <h2 style={{ color: color }} className='text-lg md:text-xl 2xl:text-2xl tracking-wide'>Player {player}'s Chance</h2>
+        <div className={`absolute md:top-12 md:left-1/2 md:-translate-x-1/2 xl:top-18 xl:left-auto xl:translate-x-0 xl:right-6 md:w-80 xl:w-92 flex flex-col items-center border border-gray-500 md:rounded-xl xl:rounded-2xl md:py-8 xl:py-8 w-68 py-8 top-16 rounded-xl`}>
+            <h2 style={{ color: color }} className='text-lg md:text-xl xl:text-2xl tracking-wide'>Player {player}'s Chance</h2>
 
-            <div className='hidden md:hidden 2xl:flex 2xl:items-center'>
+            <div className='hidden md:hidden xl:flex xl:items-center'>
                 <input type="number" min={1} max={7}
                     placeholder='Enter column no. (1-7)'
                     className='w-48 p-2 mt-12 rounded border border-white'
@@ -32,16 +32,16 @@ export default function CurrentPlayerDisplay({ player, color, handleChance, aler
                     Enter
                 </button>
             </div>
-            <span className=' mt-2 md:mt-4 2xl:mt-6 text-gray-400'><span className='hidden md:hidden 2xl:inline'>or </span>choose column no. from</span>
-            <div className="flex gap-3 md:gap-4 md:mt-5 2xl:mt-6 mt-4">
+            <span className=' mt-2 md:mt-4 xl:mt-6 text-gray-400'><span className='hidden md:hidden xl:inline'>or </span>choose column no. from</span>
+            <div className="relative flex gap-3 md:gap-4 md:mt-5 xl:mt-6 mt-4">
                 {[1, 2, 3, 4, 5, 6, 7].map((num) => (
-                    <div key={num} className="text-base md:w-7 md:h-7 2xl:w-8 2xl:h-8 w-6 h-6 md:text-sm 2xl:text-sm rounded-sm border border-white flex items-center justify-center cursor-pointer hover:bg-white hover:text-black"
+                    <div key={num} className="text-base md:w-7 md:h-7 xl:w-8 xl:h-8 w-6 h-6 md:text-sm xl:text-sm rounded-sm border border-white flex items-center justify-center cursor-pointer hover:bg-white hover:text-black"
                         onClick={() => handleChance(num - 1)}>
                         {num}
                     </div>
                 ))}
             </div>
-            <span className='mt-4 md:mt-5 2xl:mt-6 text-red-700'>{alert}</span>
+            <span className='absolute bottom-1 mt-4 md:mt-5 xl:mt-6 text-red-700'>{alert}</span>
         </div>
     )
 }
